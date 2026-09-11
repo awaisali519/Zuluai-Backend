7e78c4f7464e8f503429296ad8e3f54798e91925
awaisali519 <aa2191744@gmail.com>
2026-09-11 20:07:15 +0500

    Update GEMINI_MODEL to version 3.6-flash

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # Gemini Model
 # -----------------------------------
 
-GEMINI_MODEL = "gemini-2.5-flash"
+GEMINI_MODEL = "gemini-3.6-flash"
 
 GEMINI_URL = (
     f"https://generativelanguage.googleapis.com/"
